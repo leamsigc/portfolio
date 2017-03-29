50942be73731053cafff336407f689397f625c45
ismael garcia <leamsigc@hotmail.com>
2017-03-29 08:16:44 -0400

    add new carpet to my main destop for udemy curses

---
+++ leamsigc/projectouno/main.js
@@ -0,0 +1,5 @@
+var name= prompt("What is your name ?");
+var age= prompt("How old are you ?");
+alert("Welcome "+ name +"<br/>"+"Your are "+age+" years old;");
+console.log("Welcome "+name );
+console.log("You are "+age+" years old");
